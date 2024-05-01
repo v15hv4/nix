861013ae79f8b92efa4b518951308ff53f675cb2
v15hv4 <vishva2912@gmail.com>
2024-05-01 15:49:59 +0530

    work: Add TLP for power management

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -54,9 +54,9 @@
   services.xserver.enable = true;
 
   # Use LightDM for login management
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "v15hv4";
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "v15hv4";
 
   # Configure keymap in X11
   services.xserver = {
@@ -264,6 +264,28 @@
     rootless = {
       enable = true;
       setSocketVariable = true;
+    };
+  };
+
+
+  # power management
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      # Optional: helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
     };
   };
 }
