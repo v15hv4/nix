ec1b287b3c96d29789b70a9d22620f25a98e186d
v15hv4 <vishva2912@gmail.com>
2024-05-03 13:12:09 +0530

    flake: Add shaman inputs

--- flake.nix
+++ flake.nix
@@ -3,9 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    shaman.url = "github:v15hv4/shaman";
     apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
